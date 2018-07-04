d2084963460c3f62b2cbf62d27684ffd860e1576
hewj <wenjunhe@ctrip.com>
2018-07-04 11:46:14 +0800

    react-app

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import Form from './components/lesson1/index';
+// import App from './components/lesson2/index';
+// import LifeCycle from './components/lesson3/index';
+// import BindEvent from './components/lesson4/index';
+// import PageRouter from './components/lesson5/index';
+import Fetch from './components/lesson6/index';
+
+ReactDOM.render(
+    <Fetch />,
+    document.getElementById('root')
+);
+
